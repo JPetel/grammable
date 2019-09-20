a2e9db74a763ff7d9597179c77451a9b6e529a34
JPetel <Jacob.Petel@outlook.com>
2019-09-20 01:21:43 -0400

    editing and updating gram now working

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,10 +4,30 @@ class GramsController < ApplicationController
   def index
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def show
     @gram = Gram.find_by_id(params[:id])
     if @gram.blank?
-      render plain: 'Not Found', status: :not_found
+      return render_not_found if @gram.blank?
+    end
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      return render_not_found if @gram.blank?
     end
   end
 
@@ -31,4 +51,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
+  def render_not_found
+    render plain: 'Not Found', status: :not_found
+  end
 end
