38224143d69ed450520338a8c3d1f75b422e3169
JPetel <Jacob.Petel@outlook.com>
2019-09-20 01:35:21 -0400

    user can now delete grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,9 +2,25 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  #---------------GRAM DESTROY ------------#
+  describe "grams#destroy action" do
+    it "should allow a user to destroy a gram" do
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: {id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it "should return a 404 error message if we cannot find the gram the user is looking for" do
+      delete :destroy, params: {id: 'random message'}
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
 
   #---------------- GRAM UPDATE -----------#
-  describe "gram#update action" do
+  describe "grams#update action" do
     it "should allow users to update grams" do
       gram = FactoryBot.create(:gram, message: "Initial Value")
       patch :update, params: {id: gram.id, gram: {message: 'Changed'}}
@@ -28,7 +44,7 @@ RSpec.describe GramsController, type: :controller do
   end
 
   #----------------GRAM EDIT ---------------#
-  describe "gram#edit action" do
+  describe "grams#edit action" do
     it "should successfully show the edit form if gram is found" do
       gram = FactoryBot.create(:gram)
       get :edit, params: {id: gram.id }
