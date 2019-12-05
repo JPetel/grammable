c8dbbd62fbcfd42804417389450d746208b682ae
Jpetel <Jacob.Petel@outlook.com>
2019-12-05 13:26:59 -0500

    Updating css on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
     render plain: "#{status.to_s.titleize} :(", status: status
   end
 
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
 end
