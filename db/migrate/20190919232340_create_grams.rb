1b73c86f5c56c08dd8dc30e6ba4e38972eaf6605
Jpetel <Jacob.Petel@outlook.com>
2019-12-06 01:17:08 -0500

    added additional CSS to the main page, trying to fix error with empty comment being allowed

--- db/migrate/20190919232340_create_grams.rb
+++ db/migrate/20190919232340_create_grams.rb
@@ -1,7 +1,8 @@
 class CreateGrams < ActiveRecord::Migration[5.2]
   def change
     create_table :grams do |t|
-      t. text :message
+      t.text :message
+      t.string :username
       t.timestamps
     end
   end
